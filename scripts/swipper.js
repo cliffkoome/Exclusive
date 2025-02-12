d231f349c1f61c4331a53cd26ff0365857ba8605
cliffkoome <koomekoome902@gmail.com>
2025-02-12 15:58:56 +0300

    Fixed pagination and removed unncessary swiper propeties

--- scripts/swipper.js
+++ scripts/swipper.js
@@ -1,37 +1,19 @@
 const swiper = new Swiper('.swiper', {
   direction: 'horizontal',
-
-  grid: {
-    rows: 1,
-  },
-
-  fill: 'row',
-
-  loop: false,
-
+  loop: true,
   pagination: {
     el: '.swiper-pagination',
+    clickable: true,
   },
-
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
   autoplay: {
     delay: 5000,
   },
-
 });
 
 const productsSwiper = new Swiper('.product-swiper', {
   slidesPerView: 5.5,
   direction: 'horizontal',
   loop: false,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
   grid: {
     rows: 1,
     fill: 'row',
