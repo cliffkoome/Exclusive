96b5206a9481600321d0c1508a993f3385e3b206
cliffkoome <koomekoome902@gmail.com>
2025-02-12 15:47:37 +0300

    Added all sections layout and completed todays section

--- scripts/swipper.js
+++ scripts/swipper.js
@@ -1,7 +1,13 @@
 const swiper = new Swiper('.swiper', {
   direction: 'horizontal',
 
-  loop: true,
+  grid: {
+    rows: 1,
+  },
+
+  fill: 'row',
+
+  loop: false,
 
   pagination: {
     el: '.swiper-pagination',
@@ -12,12 +18,31 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   },
 
-  // scrollbar: {
-  //   el: '.swiper-scrollbar',
-  // },
-
   autoplay: {
     delay: 5000,
   },
 
+});
+
+const productsSwiper = new Swiper('.product-swiper', {
+  slidesPerView: 5.5,
+  direction: 'horizontal',
+  loop: false,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  grid: {
+    rows: 1,
+    fill: 'row',
+  },
+})
+
+
+document.querySelector('.js-arrow-right').addEventListener('click', () => {
+  productsSwiper.slideNext();
+});
+
+document.querySelector('.js-arrow-left').addEventListener('click', () => {
+  productsSwiper.slidePrev();
 });
