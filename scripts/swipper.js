80e0b617aa07f42908f69c8d6277dbe6c06bbb4c
cliffkoome <koomekoome902@gmail.com>
2025-02-13 14:47:12 +0300

    Completed all the sections and made like button interactive

--- scripts/swipper.js
+++ scripts/swipper.js
@@ -18,13 +18,43 @@ const productsSwiper = new Swiper('.product-swiper', {
     rows: 1,
     fill: 'row',
   },
+});
+
+const productsSwiper2  = new Swiper('.product-swipper-2', {
+  slidesPerView: 4,
+  direction: 'horizontal',
+  loop: false,
+  grid: {
+    rows: 2,
+    fill: 'row',
+  },
+  spaceBetween: 20,
 })
 
-
-document.querySelector('.js-arrow-right').addEventListener('click', () => {
+document.querySelector('.js-today-arrow-right').addEventListener('click', () => {
   productsSwiper.slideNext();
 });
 
-document.querySelector('.js-arrow-left').addEventListener('click', () => {
+document.querySelector('.js-today-arrow-left').addEventListener('click', () => {
   productsSwiper.slidePrev();
+});
+
+document.querySelector('.js-products-arrow-right').addEventListener('click', () => {
+  productsSwiper2.slideNext();
+});
+
+document.querySelector('.js-products-arrow-left').addEventListener('click', () => {
+  productsSwiper2.slidePrev();
+});
+
+const heartElement = document.querySelectorAll('.like-btn .fa-heart');
+
+heartElement.forEach(button => {
+  button.addEventListener('click', () => {
+    if (button.classList.contains('fa-heart-clicked')) {
+      button.classList.remove('fa-heart-clicked');
+    } else {
+      button.classList.add('fa-heart-clicked');
+    }
+  });
 });
