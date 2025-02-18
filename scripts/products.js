6f0824fa43961d056b64243a59cc59fe2a2eb75c
cliffkoome <koomekoome902@gmail.com>
2025-02-18 16:00:56 +0300

    Created Products page

---
+++ scripts/products.js
@@ -0,0 +1,11 @@
+const heartElement = document.querySelectorAll('.like-btn .fa-heart');
+
+heartElement.forEach(button => {
+  button.addEventListener('click', () => {
+    if (button.classList.contains('fa-heart-clicked')) {
+      button.classList.remove('fa-heart-clicked');
+    } else {
+      button.classList.add('fa-heart-clicked');
+    }
+  });
+});
