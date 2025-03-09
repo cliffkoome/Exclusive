1f0211e849bb61dc661364755ed8c3a185dfaae3
Clifford Karani <koomekoome902@gmail.com>
2025-03-09 11:38:42 +0300

    Generate home page products

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,8 @@
 import { productsSwiper, productsSwiper2 } from "./swipper.js";
 import { products } from "../data/products.js";
+import { calculateCartItems, addToCart } from "../data/cart.js";
+
+calculateCartItems();
 
 let flashSalesHTML = '';
 let flashSaleItems = 0;
@@ -13,17 +16,17 @@ products.forEach((item) => {
   const image = item.image;
 
   if (discount >= 0.35) {
-    flashSaleItems ++;
+    flashSaleItems++;
     flashSalesHTML +=
-  `
+      `
     <div class="swiper-slide">
       <div class="item-card">
         <div class="item-card-image">
           <img src="${image}" class="item-card-img">
           <div class="discount">
-            -${(discount*100).toFixed(0)}%
+            -${(discount * 100).toFixed(0)}%
           </div>
-          <div class="add-to-cart">
+          <div class="add-to-cart" data-product-id="${productId}">
             Add To Cart
           </div>
           <div class="like-btn">
@@ -35,8 +38,8 @@ products.forEach((item) => {
         </div>
         <div class="item-card-info">
           <div class="item-card-info-name">${name}</div>
-          <div class="item-card-info-price">$${((priceCents/100)-((priceCents/100)*discount)).toFixed(2)}<span style="text-decoration: line-through; margin-left: 10px; color: gray;">${priceCents/100}</span></div>
-          <div class="item-card-info-rating">Rating: ${rating*0.1}</div>
+          <div class="item-card-info-price">$${((priceCents / 100) - ((priceCents / 100) * discount)).toFixed(2)}<span style="text-decoration: line-through; margin-left: 10px; color: gray;">${priceCents / 100}</span></div>
+          <div class="item-card-info-rating">Rating: ${rating * 0.1}</div>
         </div>
       </div>
     </div>
@@ -45,10 +48,99 @@ products.forEach((item) => {
 });
 
 if (flashSaleItems === 0) {
-  document.querySelector('.today-section').innerHTML ='';
+  document.querySelector('.today-section').innerHTML = '';
 }
 
 document.querySelector('.js-flash-sales').innerHTML = flashSalesHTML;
+
+let bestSellingHTML = '';
+let bestSellingItems = 0;
+
+products.forEach(item => {
+  const productId = item.productId;
+  const name = item.name;
+  const priceCents = item.priceCents;
+  const discount = item.discount;
+  const rating = item.rating;
+  const image = item.image;
+
+  if (rating >= 50 && bestSellingItems <= 4) {
+    bestSellingHTML +=
+      `
+      <div class="item-card-2">
+        <div class="item-card-image">
+          <img src="${image}" class="item-card-img">
+          <div class="discount">
+            -${(discount * 100).toFixed(0)}%
+          </div>
+          <div class="add-to-cart" data-product-id="${productId}">
+            Add To Cart
+          </div>
+          <div class="like-btn">
+            <i class="fa-solid fa-heart"></i>
+          </div>
+          <div class="view-btn">
+            <i class="fa-solid fa-eye"></i>
+          </div>
+        </div>
+        <div class="item-card-info">
+          <div class="item-card-info-name">${name}</div>
+          <div class="item-card-info-price">$${((priceCents / 100) - ((priceCents / 100) * discount)).toFixed(2)}<span style="text-decoration: line-through; margin-left: 10px; color: gray;">${priceCents / 100}</span></div>
+          <div class="item-card-info-rating">Rating: ${rating * 0.1}</div>
+        </div>
+      </div>
+    `
+    bestSellingItems++;
+  }
+})
+
+document.querySelector('.this-month-bottom').innerHTML = bestSellingHTML;
+
+
+let exploreProductsHTML = '';
+let exploreProductsCounter = 0;
+
+products.forEach(item => {
+  const productId = item.productId;
+  const name = item.name;
+  const priceCents = item.priceCents;
+  const discount = item.discount;
+  const rating = item.rating;
+  const image = item.image;
+
+  if (exploreProductsCounter <= 15) {
+    exploreProductsHTML +=
+      `
+    <div class="swiper-slide">
+      <div class="item-card">
+        <div class="item-card-image">
+          <img src="${image}" class="item-card-img">
+          <div class="discount">
+            -${(discount * 100).toFixed(0)}%
+          </div>
+          <div class="add-to-cart" data-product-id="${productId}">
+            Add To Cart
+          </div>
+          <div class="like-btn">
+            <i class="fa-solid fa-heart"></i>
+          </div>
+          <div class="view-btn">
+            <i class="fa-solid fa-eye"></i>
+          </div>
+        </div>
+        <div class="item-card-info">
+          <div class="item-card-info-name">${name}</div>
+          <div class="item-card-info-price">$${((priceCents / 100) - ((priceCents / 100) * discount)).toFixed(2)}<span style="text-decoration: line-through; margin-left: 10px; color: gray;">${priceCents / 100}</span></div>
+          <div class="item-card-info-rating">Rating: ${rating * 0.1}</div>
+        </div>
+      </div>
+    </div>
+    `
+    exploreProductsCounter++;
+  }
+});
+
+document.querySelector('.js-explore-products').innerHTML = exploreProductsHTML;
 
 const heartElement = document.querySelectorAll('.like-btn .fa-heart');
 
@@ -84,3 +176,10 @@ document.querySelector('.js-products-arrow-right').addEventListener('click', () 
 document.querySelector('.js-products-arrow-left').addEventListener('click', () => {
   productsSwiper2.slidePrev();
 });
+
+document.querySelectorAll('.add-to-cart').forEach((product) => {
+  const { productId } = product.dataset;
+  product.addEventListener('click', () => {
+    addToCart(productId);
+  })
+})
